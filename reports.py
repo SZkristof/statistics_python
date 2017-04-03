bb2d96cc6c4e7d719091755f77b881e6be72c9a0
SZkristof <szabokristof0@gmail.com>
2017-04-03 19:59:27 +0200

    Done until mid 3rd Qstn

--- reports.py
+++ reports.py
@@ -1,2 +1,21 @@
+def count_games(file_name):
+    with open(file_name) as file:
+        for i, line in enumerate(file):
+            pass
+    return (i + 1)
 
-# Report functions
+
+def decide(file_name, year):
+    with open(file_name) as file:
+        for line in file:
+            if str(year) in line:
+                return True
+        return False
+
+
+def get_latest(file_name):
+    with open(file_name) as f:
+        data_list = f.read().splitlines()
+        data_list = [item.split('\t') for item in data_list]
+        #lets continue here
+
